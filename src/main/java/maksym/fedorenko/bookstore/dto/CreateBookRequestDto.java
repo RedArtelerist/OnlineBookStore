ef23aebc0a3503be90d746ad24b159050b62cefb
Maksym Fedorenko <fedorenko.max02@gmail.com>
2023-10-17 11:11:42 +0300

    converted dto classes to records

--- src/main/java/maksym/fedorenko/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/maksym/fedorenko/bookstore/dto/CreateBookRequestDto.java
@@ -3,30 +3,13 @@ package maksym.fedorenko.bookstore.dto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
-import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-@Data
-public class CreateBookRequestDto {
-    @NotNull
-    @Length(max = 128)
-    private String title;
-
-    @NotNull
-    @Length(max = 128)
-    private String author;
-
-    @NotNull
-    @Length(max = 128)
-    private String isbn;
-
-    @NotNull
-    @Positive
-    private BigDecimal price;
-
-    @Length(max = 1024)
-    private String description;
-
-    @Length(max = 128)
-    private String coverImage;
+public record CreateBookRequestDto(
+        @NotNull @Length(max = 128) String title,
+        @NotNull @Length(max = 128) String author,
+        @NotNull @Length(max = 128) String isbn,
+        @NotNull @Positive BigDecimal price,
+        @Length(max = 1024) String description,
+        @Length(max = 128) String coverImage) {
 }
--- src/main/java/maksym/fedorenko/bookstore/dto/UpdateBookRequestDto.java
+++ src/main/java/maksym/fedorenko/bookstore/dto/UpdateBookRequestDto.java
@@ -2,30 +2,17 @@ package maksym.fedorenko.bookstore.dto;
 
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
-import lombok.Data;
 import maksym.fedorenko.bookstore.model.Book;
 import org.hibernate.validator.constraints.Length;
 
-@Data
-public class UpdateBookRequestDto {
-    @Length(max = 128)
-    private String title;
-
-    @Length(max = 128)
-    private String author;
-
-    @Length(max = 128)
-    private String isbn;
-
-    @Positive
-    private BigDecimal price;
-
-    @Length(max = 1024)
-    private String description;
-
-    @Length(max = 128)
-    private String coverImage;
-
+public record UpdateBookRequestDto(
+        @Length(max = 128) String title,
+        @Length(max = 128) String author,
+        @Length(max = 128) String isbn,
+        @Positive BigDecimal price,
+        @Length(max = 1024) String description,
+        @Length(max = 128) String coverImage
+) {
     public void updateBook(Book book) {
         if (title != null && !title.isBlank()) {
             book.setTitle(title);
