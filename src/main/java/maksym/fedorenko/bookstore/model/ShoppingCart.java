0b9ca41818b05edaab1198831a33f13d2578d869
Maksym Fedorenko <fedorenko.max02@gmail.com>
2023-10-27 20:22:14 +0300

    updated cascade type for cart items

--- src/main/java/maksym/fedorenko/bookstore/model/ShoppingCart.java
+++ src/main/java/maksym/fedorenko/bookstore/model/ShoppingCart.java
@@ -34,7 +34,7 @@ public class ShoppingCart {
 
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
-    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.REMOVE)
     private Set<CartItem> cartItems = new HashSet<>();
 
     @Column(name = "is_deleted", nullable = false)
